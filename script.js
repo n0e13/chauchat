0deb9ee1768d16cf1eaef506ced3689f6692f813
n0e13 <noemy.garcia@gmail.com>
2022-02-20 10:19:24 +0100

    terminado

---
+++ script.js
@@ -0,0 +1,19 @@
+let cargador = Math.floor(Math.random() * 400);
+console.log(cargador);
+
+function chauchat (numDisparos) {
+  for (let i=0; i < numDisparos; i++) { 
+    let pillada = Math.floor(Math.random() * 100);
+    if(pillada < 80){
+      console.log("Illo, me he quedao pillá!");
+      break;
+    }
+    if (i%4==0) {
+      console.log(" ");
+    } else {
+      console.log("pium!");
+    }
+  } 
+}
+
+chauchat(cargador);
